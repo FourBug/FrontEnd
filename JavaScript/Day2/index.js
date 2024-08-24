e8dbbf50b2022e8aa97db72111968647bf87f5f1
Mukesh <mukeshtopcoder@gmail.com>
2024-08-24 21:01:02 +0530

    Day2

---
+++ JavaScript/Day2/index.js
@@ -0,0 +1,74 @@
+// console.log("Hello World!\nHello Mumbai!");
+// console.log("Hello India!");
+
+/*
+Java Script print data in colsole window from console.log()
+and on the webpage from document.write()
+Variable Creation 
+Varibale Name limit : 256 Character
+Recommended : 10-15 Character
+
+Keyword used in creation of varible
+var     let     const
+
+Scope of Variable 
+
+var a;  (Global)
+var a = 10; (Global)
+let b = 20; (Local)
+const c = 30; (Constant, You can not change the data of this variable)
+
+
+*/
+
+// var a = 10;
+// let b = 20;
+// const c = 30;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// {
+//     var a = 10;  // Global Variable, You can use anywhere
+// }
+// {
+//     let b = 20;  // Local Variable, And you can use only in this scope
+// }
+// const c = 30;
+// console.log(a);
+// // console.log(b); You can print here beacuse of local varibale
+// console.log(c);
+// const pi = 3.14;
+
+
+// Write A program to Add Two Numbers.
+// let a = 10;
+// let b = 20;
+// let c = a+b;
+// console.log("Addition : "+c);
+
+// Write A Program To Calculate Total and Percentage of 5 subject
+// marks of a student
+// let math = 73;
+// let english = 67;
+// let science = 73;
+// let hindi = 72;
+// let computer = 68;
+// let total = math+english+science+hindi+computer;
+// let per = total/5;
+// console.log("Total Marks : "+total);
+// console.log("Percentage : "+per);
+
+/*
+let a = 'M'; // datatype : character
+let b = 82; // datatype : integer
+let c = "amanKumar";  // datatype : string
+let d = 72.78; // datatype : float
+let e = true; // datatype : boolean
+*/
+
+// let name = "Rahul Kumar Shrivastav";
+// let city = "Delhi";
+// let mobile = "9827612873";
+// let age = 25;
+// console.log("Hi, My name is "+name+" and I am from "+city+" my mobile number is "+mobile+" and I am "+age+" years old");
