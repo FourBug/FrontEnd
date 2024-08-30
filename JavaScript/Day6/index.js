86be8d4f7adf498757ea32e681ae967780c0d8b3
Mukesh <mukeshtopcoder@gmail.com>
2024-08-30 21:04:13 +0530

    Day6

---
+++ JavaScript/Day6/index.js
@@ -0,0 +1,22 @@
+// WAP TO PRINT TABLE OF A NUMBER
+// var num = 5;
+// for(let a=1;a<=10;a++){
+//     console.log(num+" * "+a+" = "+num*a);
+// }
+// 5 * 1 = 5
+// 5 * 2 = 10
+
+// How to create a user define function
+/*
+function function_name()
+{
+    function definiton;
+}
+*/ 
+function myFun(){
+console.log("Hello");
+}
+myFun();
+myFun();
+myFun();
+myFun();
